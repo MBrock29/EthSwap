52644f518b05d9a17be8950584b8711d050ec906
MBrock29 <55901037+MBrock29@users.noreply.github.com>
2022-03-30 09:32:39 +0100

    Initial upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,11 @@ const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
 module.exports = async function(deployer) {
-  // Deploy Token
   await deployer.deploy(Token);
-  const token = await Token.deployed()
+  const token = await Token.deployed();
 
-  // Deploy EthSwap
   await deployer.deploy(EthSwap, token.address);
-  const ethSwap = await EthSwap.deployed()
+  const ethSwap = await EthSwap.deployed();
 
-  // Transfer all tokens to EthSwap (1 million)
-  await token.transfer(ethSwap.address, '1000000000000000000000000')
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
 };
